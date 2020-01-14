273370a0c21494abbd25234b77e5f4cee5ed2ab9
Li-Yangyang <lyy0731@mail.ustc.edu.cn>
2020-01-14 12:47:51 -0500

    deltarv correction

--- src/spot.jl
+++ src/spot.jl
@@ -56,8 +56,8 @@ function calci(SpotModel::Spot, t::Array{Float64,1}, index::Int)
 
     #RV modeling
     veq = 2 * pi * SpotModel.rstar * 6.96e8 / SpotModel.period[index] / 84600.0
-    rv_rot = -1.0 * veq * sin(SpotModel.incl[index]) * cos(SpotModel.lat[index]) .* sin.(lon) .* dF
-    rv_conv = SpotModel.Q[index] * SpotModel.vconv .* mu  .* dF
+    rv_rot = veq * sin(SpotModel.incl[index]) * cos(SpotModel.lat[index]) .* sin.(lon) .* dF
+    rv_conv = -1.0 * SpotModel.Q[index] * SpotModel.vconv .* mu  .* dF
     dRV = rv_rot .+ rv_conv
     bis = dRV .* cos.(lon)
 
