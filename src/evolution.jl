8c93c6b8f8b1a2f8f6b76d8b086665cf8ab623b2
Li-Yangyang <lyy0731@mail.ustc.edu.cn>
2020-02-18 13:32:25 -0500

    add decay scale for sun simulation

--- src/evolution.jl
+++ src/evolution.jl
@@ -25,7 +25,7 @@ function sample_lat(diagram::Diagram, t::Float64)
 end
 
 function sample_lifetime(decay_scale::Float64)
-    u = MixtureModel(Uniform.([2/24*decay_scale, 2.0*decay_scale, 30.0*decay_scale], 
+    u = MixtureModel(Uniform.([2/24*decay_scale, 2.0*decay_scale, 50.0*decay_scale], 
                             [2.0*decay_scale, 30.0*decay_scale, 60.0*decay_scale]),
                             [0.0, 0.0, 1.0])
     return rand(u)
